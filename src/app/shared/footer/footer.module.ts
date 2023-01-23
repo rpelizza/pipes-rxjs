bba30a8c21135aa7d2fbec9c2077b47c8bba881f
Rafael Pedro Pelizza <rafael.pelizza@gmail.com>
2023-01-23 20:44:39 -0300

    inicio footer

--- src/app/shared/footer/footer.module.ts
+++ src/app/shared/footer/footer.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './component/footer.component';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
 	declarations: [FooterComponent],
-	imports: [CommonModule],
+	imports: [CommonModule, MatChipsModule, MatIconModule],
 	exports: [FooterComponent],
 })
 export class FooterModule {}
-
