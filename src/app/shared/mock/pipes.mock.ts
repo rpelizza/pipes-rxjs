fa3bd168f920ef7b6f6fdb28eb41c30ae3afd1bd
Rafael Pedro Pelizza <rafael.pelizza@gmail.com>
2023-01-24 08:43:38 -0300

    inicio mock

---
+++ src/app/shared/mock/pipes.mock.ts
@@ -0,0 +1,58 @@
+export const mockPipes = [
+	{
+		group: 'Combinação',
+		icon: 'merge_type',
+		description:
+			'As pipes de combinação (combination) do RxJS são operações que combinam várias fontes de dados em uma única saída',
+		pipes: [],
+	},
+	{
+		group: 'Condição',
+		icon: 'check_circle',
+		description:
+			'As pipes de condicionais (conditional) do RxJS são operações que permitem a você tomar decisões sobre como um fluxo de dados deve ser manipulado com base em determinadas condições.',
+		pipes: [],
+	},
+	{
+		group: 'Criação',
+		icon: 'add_circle',
+		description:
+			'As pipes de criação (creation) do RxJS são operações que permitem a você criar novos fluxos de dados a partir de fontes de dados existentes ou não',
+		pipes: [],
+	},
+	{
+		group: 'Tratamento de erro',
+		icon: 'error',
+		description:
+			'As pipes de tratamento de erro (Error Handling) do RxJS são operações que permitem a você lidar com erros que possam ocorrer em um fluxo de dados.',
+		pipes: [],
+	},
+	{
+		group: 'Transmição de dados',
+		icon: 'send',
+		description:
+			'As pipes de transmição de dados (Multicasting) do RxJS são operações que permitem a você compartilhar um fluxo de dados entre vários observadores. Com elas, é possível evitar que uma fonte de dados seja compartilhada entre vários observadores, o que pode economizar recursos e melhorar o desempenho.',
+		pipes: [],
+	},
+	{
+		group: 'Filtragem',
+		icon: 'filter_list',
+		description:
+			'As pipes de filtragem (filtering) do RxJS são operações que permitem a você filtrar valores emitidos por um fluxo de dados com base em determinadas condições.',
+		pipes: [],
+	},
+	{
+		group: 'Utility',
+		icon: 'build',
+		description:
+			'As pipes de utilidade (utility) do RxJS são operações que permitem a você realizar tarefas comuns, como aguardar a conclusão de um fluxo de dados ou executar uma função para cada valor emitido por um fluxo de dados.',
+		pipes: [],
+	},
+	{
+		group: 'Subjects',
+		icon: 'code',
+		description:
+			'Os Subjects no RxJS são objetos que implementam tanto a interface de fluxo de dados quanto a interface de observador. Isso significa que eles podem emitir valores e também podem ser inscritos em outros fluxos de dados.',
+		subjectTypes: [],
+	},
+];
