b2860a19e34dc40f9633cde8678cb251e5cc2533
Rafael Pedro Pelizza <rafael.pelizza@gmail.com>
2023-01-25 10:39:35 -0300

    buscando dados da pipe

--- src/app/components/pipes/component/pipes.component.ts
+++ src/app/components/pipes/component/pipes.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PipesInterface, RxjsInterface } from 'src/app/shared/interfaces/rxjs.interface';
+import { mockPipes } from 'src/app/shared/mock/pipes.mock';
 
 @Component({
 	selector: 'app-pipes',
 	templateUrl: './pipes.component.html',
 	styleUrls: ['./pipes.component.scss'],
 })
-export class PipesComponent {}
+export class PipesComponent implements OnInit {
+	private mockedPipes: Array<RxjsInterface> = mockPipes as Array<RxjsInterface>;
+	public pipeFound: Array<PipesInterface> = [];
 
+	constructor(private readonly _route: ActivatedRoute) {}
+
+	ngOnInit(): void {
+		this.pipeFound = this.getPipeById(this.getIdFromRoute());
+		console.log('objeto encontrado', this.pipeFound);
+	}
+
+	private getIdFromRoute(): string {
+		return this._route.snapshot.paramMap.get('id') as string;
+	}
+
+	private getPipeById(id: string): Array<PipesInterface> {
+		this.pipeFound = [];
+		this.mockedPipes.forEach((theme) => {
+			theme.pipes.forEach((pipe) => {
+				if (pipe.pipeId === id) {
+					const newObj = {
+						group: theme.group,
+						description: theme.description,
+						icon: theme.icon,
+						...pipe,
+					};
+					this.pipeFound.push(newObj);
+				}
+			});
+		});
+		return this.pipeFound;
+	}
+}
