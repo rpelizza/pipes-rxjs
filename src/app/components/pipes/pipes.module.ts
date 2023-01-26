861b15a53c283619902fc26d8b544753bdc52269
Rafael Pedro Pelizza <rafael.pelizza@gmail.com>
2023-01-26 11:21:02 -0300

    add link

--- src/app/components/pipes/pipes.module.ts
+++ src/app/components/pipes/pipes.module.ts
@@ -9,6 +9,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
 	declarations: [PipesComponent],
@@ -20,6 +21,7 @@ import { MatDividerModule } from '@angular/material/divider';
 		MatButtonModule,
 		MatIconModule,
 		MatDividerModule,
+		MatTooltipModule,
 	],
 })
 export class PipesModule {}
