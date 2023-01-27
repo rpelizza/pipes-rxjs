cff0869bb1e4907973960caa9c54945f4a1f5c88
Rafael Pedro Pelizza <rafael.pelizza@gmail.com>
2023-01-27 10:30:23 -0300

    footer ok

--- src/app/shared/components/footer/component/footer.component.ts
+++ src/app/shared/components/footer/component/footer.component.ts
@@ -6,5 +6,12 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 	styleUrls: ['./footer.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent {}
-
+export class FooterComponent {
+	public redirectToExternalUrl(url: string, isEmail?: boolean): void {
+		if (isEmail) {
+			window.open(`mailto:${url}`, '_blank');
+		} else {
+			window.open(url, '_blank');
+		}
+	}
+}
